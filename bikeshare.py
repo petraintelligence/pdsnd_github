63d520f18f50dba0e3a4fe3aebcb7cfeb65d13d4
petraintelligence <jsbrown@burnsmcd.com>
2020-05-24 18:52:02 -0500

    Changed greeting in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -37,7 +37,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Good day Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         while True:
